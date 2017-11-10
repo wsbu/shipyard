24c196c0c3e2cf8ef5ba056f6362e73311566e61
Paul Walker <paul.walker@redlion.net>
2017-11-10 15:15:05 -0600

    linaro/Dockerfile: added WSBU_TOOLCHAIN_PATH and split wget line for readability

--- linaro/linaro-4.9.4.cmake
+++ linaro/linaro-4.9.4.cmake
@@ -1,6 +1,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
-set(CMAKE_C_COMPILER   "${CMAKE_CURRENT_SOURCE_DIR}/linaro-4.9.4/bin/arm-linux-gnueabihf-gcc")
-set(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/linaro-4.9.4/bin/arm-linux-gnueabihf-g++")
+set(CMAKE_C_COMPILER   "$ENV{WSBU_TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-gcc")
+set(CMAKE_CXX_COMPILER "$ENV{WSBU_TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-g++")
 
-list(APPEND CMAKE_FIND_ROOT_PATH "${CMAKE_CURRENT_SOURCE_DIR}/linaro-4.9.4/arm-linux-gnueabihf")
+list(APPEND CMAKE_FIND_ROOT_PATH "$ENV{WSBU_TOOLCHAIN_PATH}/arm-linux-gnueabihf")
