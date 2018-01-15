50eee6cc9d2a52bdd42452fe6cad593dc1feb1a0
David Zemon <david@zemon.name>
2018-01-14 22:39:32 -0600

    Update for Conan

--- linaro/linaro-4.9.4.cmake
+++ linaro/linaro-4.9.4.cmake
@@ -1,6 +1,25 @@
 set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER   "$ENV{WSBU_TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-gcc")
-set(CMAKE_CXX_COMPILER "$ENV{WSBU_TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-g++")
+set(CMAKE_C_COMPILER   "$ENV{GCC_PREFIX}/bin/arm-linux-gnueabihf-gcc")
+set(CMAKE_CXX_COMPILER "$ENV{GCC_PREFIX}/bin/arm-linux-gnueabihf-g++")
+set(CMAKE_AR           "$ENV{GCC_PREFIX}/bin/arm-linux-gnueabihf-ar")
+set(CMAKE_RANLIB       "$ENV{GCC_PREFIX}/bin/arm-linux-gnueabihf-ranlib")
+set(CMAKE_OBJCOPY      "$ENV{GCC_PREFIX}/bin/arm-linux-gnueabihf-objcopy")
+set(CMAKE_OBJDUMP      "$ENV{GCC_PREFIX}/bin/arm-linux-gnueabihf-objdump")
 
-list(APPEND CMAKE_FIND_ROOT_PATH "$ENV{WSBU_TOOLCHAIN_PATH}/arm-linux-gnueabihf")
+set(CMAKE_FIND_ROOT_PATH "$ENV{GCC_PREFIX}/arm-linux-gnueabihf")
+
+# Discard path returned by pkg-config and associated with HINTS in module
+# like FindOpenSSL.
+set(CMAKE_IGNORE_PATH "/usr/lib/x86_64-linux-gnu/" "/usr/lib/x86_64-linux-gnu/lib/")
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+
+set(CMAKE_CROSSCOMPILING_EMULATOR "$ENV{EMULATOR}")
