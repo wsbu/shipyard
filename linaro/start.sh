2822d66041ffc54024161a2443b3b16b9ae167f7
David Zemon <david.zemon@redlion.net>
2018-01-19 14:14:54 -0600

    Allow cloning from Git

---
+++ linaro/start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ "${uid}" -a "${gid}" ] ; then
+    set -e
+    user_name=$(basename ${HOME})
+    groupadd --gid ${gid} ${user_name}
+    useradd \
+        --home-dir "$HOME" \
+        --uid ${uid} \
+        --gid ${gid} \
+        --groups sudo \
+        ${user_name}
+    chown ${uid}:${gid} $HOME
+    chown ${uid}:${gid} $HOME/.ssh
+    su_cmd="sudo -u ${user_name}"
+    set +e
+fi
+
+if (( $# == 0 )); then
+    set sdf-e
+	${su_cmd} /bin/bash
+else
+    set -e
+	${su_cmd} "$@"
+fi
